d53dc89e2b0d778fddcde5e7822486c4661bbd2d
Wellyngton Antonio Kuhn <94876959+Wellyngtonkuhn@users.noreply.github.com>
2023-07-05 13:43:05 -0300

    feat: update database schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import { prisma } from "../lib/prisma";
-
-const getData = async () => {
-  const trips = await prisma.trip.findMany({});
-  return trips;
-};
-
 export default async function Home() {
-  const data = await getData();
-  console.log({ data });
   return <main>Home</main>;
 }
